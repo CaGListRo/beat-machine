78536ed6bde33b4235d217e1cd14ccfb44a7998f
CaGListRo <paco.nyns@gmail.com>
2024-05-02 16:05:07 +0200

    WIP: completed the measurments and positioning and size calculations. Made the beat button work. Implemented play, pause, stop and bpm buttons. Implemented slot indicators.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import load_image, scale_image, create_beat_button_pattern, Button
+from utils import load_image, scale_image, create_beat_button_pattern, create_slot_light_list, Button
 
 import pygame as pg
 from time import time
@@ -15,8 +15,9 @@ class BeatMachine:
         self.active_instrument_slot: int = 0
         self.beat_duration: float = self.calculate_beat_times()
         self.beat_time: float = 0
-        self.play: bool = False
+        self.state: str = "stop"
         self.shift: bool = False
+        self.font = pg.font.SysFont("arial", 40)
 
         self.dt: float = 0
         self.last_time: float = time()
@@ -29,31 +30,43 @@ class BeatMachine:
         
         self.assets: dict = {
             "body": scale_image(load_image(img_name="body"), scalefactor=0.55),
-            "bpm +- 1": scale_image(load_image(img_name="bpm plus minus 1"), scalefactor=0.55),
-            "bpm +- 10": scale_image(load_image(img_name="bpm plus minus 10"), scalefactor=0.55),
-            "button1/active": scale_image(load_image(img_name="button 1 active"), scalefactor=0.55),
-            "button1/inactive": scale_image(load_image(img_name="button 1 inactive"), scalefactor=0.55),
-            "button234/active": scale_image(load_image(img_name="button 234 active"), scalefactor=0.55),
-            "button234/inactive": scale_image(load_image(img_name="button 234 inactive"), scalefactor=0.55),
-            "pause/active": scale_image(load_image(img_name="button pause active"), scalefactor=0.55),
-            "pause/inactive": scale_image(load_image(img_name="button pause inactive"), scalefactor=0.55),
-            "play/active": scale_image(load_image(img_name="button play active"), scalefactor=0.55),
-            "play/inactive": scale_image(load_image(img_name="button play inactive"), scalefactor=0.55),
-            "stop/active": scale_image(load_image(img_name="button stop active"), scalefactor=0.55),
-            "stop/inactive": scale_image(load_image(img_name="button stop inactive"), scalefactor=0.55),
-            "slider knob": scale_image(load_image(img_name="slider knob"), scalefactor=0.55),
-            "slot light/active": scale_image(load_image(img_name="slot indicator active"), scalefactor=0.55),
-            "slot light/inactive": scale_image(load_image(img_name="slot indicator inactive"), scalefactor=0.55),
+            "bpm +- 1": scale_image(load_image(img_name="bpm plus minus 1"), scalefactor=0.36),
+            "bpm +- 10": scale_image(load_image(img_name="bpm plus minus 10"), scalefactor=0.36),
+            "button1/active": scale_image(load_image(img_name="button 1 active"), scalefactor=0.42),
+            "button1/inactive": scale_image(load_image(img_name="button 1 inactive"), scalefactor=0.42),
+            "button234/active": scale_image(load_image(img_name="button 234 active"), scalefactor=0.42),
+            "button234/inactive": scale_image(load_image(img_name="button 234 inactive"), scalefactor=0.42),
+            "pause/active": scale_image(load_image(img_name="button pause active"), scalefactor=0.36),
+            "pause/inactive": scale_image(load_image(img_name="button pause inactive"), scalefactor=0.36),
+            "play/active": scale_image(load_image(img_name="button play active"), scalefactor=0.36),
+            "play/inactive": scale_image(load_image(img_name="button play inactive"), scalefactor=0.36),
+            "stop/active": scale_image(load_image(img_name="button stop active"), scalefactor=0.36),
+            "stop/inactive": scale_image(load_image(img_name="button stop inactive"), scalefactor=0.36),
+            "slider knob": scale_image(load_image(img_name="slider knob"), scalefactor=0.49),
+            "slot light/active": scale_image(load_image(img_name="slot indicator active"), scalefactor=0.36),
+            "slot light/inactive": scale_image(load_image(img_name="slot indicator inactive"), scalefactor=0.36),
+            "display_glas": scale_image(load_image(img_name="display glas"), scalefactor=0.44),
         }
-        
+
         self.beat_buttons = create_beat_button_pattern(self)
+        self.slot_lights = create_slot_light_list(self)
+
+        self.play_button = Button(prog=self, button_type="play", pos=(988, 31), activatable=True)
+        self.pause_button = Button(prog=self, button_type="pause", pos=(931, 31), activatable=True)
+        self.stop_button = Button(prog=self, button_type="stop", pos=(882, 31), activatable=True)
+        self.bpm_minus_ten_button = Button(prog=self, button_type="bpm +- 10", pos=(470, 31), mirror=True)
+        self.bpm_minus_one_button = Button(prog=self, button_type="bpm +- 1", pos=(519, 31), mirror=True)
+        self.bpm_plus_ten_button = Button(prog=self, button_type="bpm +- 10", pos=(735, 31))
+        self.bpm_plus_one_button = Button(prog=self, button_type="bpm +- 1", pos=(697, 31))
         
     def calculate_beat_times(self) -> float:
         return 60 / (self.bpm * 4)
     
+    def de_activate_slot_lights(self) -> None:
+        self.slot_lights[self.active_instrument_slot].update(activated=True)
+
     def slot_shifter(self) -> None:
         if self.shift:
-            print(self.active_instrument_slot)
             self.active_instrument_slot += 1
             self.shift = False
             if self.active_instrument_slot > self.number_of_beats - 1:
@@ -81,22 +94,67 @@ class BeatMachine:
         self.dt = time() - self.last_time
         self.last_time = time()
 
-    def draw_window(self) -> None:
-        pg.display.set_caption(f"     Beat Maschine     FPS: {self.fps}")
-        self.screen.blit(self.assets["body"], (0,0))
+    def check_button_collisions(self):
+        for button_list in self.beat_buttons:
+            for btn in button_list:
+                btn.check_collision()
+        if self.play_button.check_collision():
+            if self.pause_button.is_active(): self.pause_button.switch_state()
+            if self.stop_button.is_active(): self.stop_button.switch_state()
+            self.state = "play"
+        if self.pause_button.check_collision():
+            if self.play_button.is_active(): self.play_button.switch_state()
+            if self.stop_button.is_active(): self.stop_button.switch_state()
+            self.state = "pause"
+        if self.stop_button.check_collision():
+            if self.pause_button.is_active(): self.pause_button.switch_state()
+            if self.play_button.is_active(): self.play_button.switch_state()
+            self.state = "stop"
+            self.active_instrument_slot = 0
+        if self.bpm_minus_ten_button.check_collision(): self.bpm -= 10
+        if self.bpm_minus_one_button.check_collision(): self.bpm -= 1
+        if self.bpm_plus_ten_button.check_collision(): self.bpm += 10
+        if self.bpm_plus_one_button.check_collision(): self.bpm += 1
 
+    def render_buttons(self):
+        for button_list in self.beat_buttons:
+            for btn in button_list:
+                btn.render(self.screen)
+        for light in self.slot_lights:
+            light.render(self.screen)
+        self.play_button.render(self.screen)
+        self.pause_button.render(self.screen)
+        self.stop_button.render(self.screen)
+        self.bpm_minus_ten_button.render(self.screen)
+        self.bpm_minus_one_button.render(self.screen)
+        self.bpm_plus_ten_button.render(self.screen)
+        self.bpm_plus_one_button.render(self.screen)
+
+    def draw_window(self) -> None:
+        pg.display.set_caption(f"     Beat Machine     FPS: {self.fps}")
+        self.screen.blit(self.assets["body"], (0,0))
+        self.render_buttons()
+        
+        bpm_to_blit = self.font.render(str(self.bpm), False, "green")
+        self.screen.blit(bpm_to_blit, (680 - bpm_to_blit.get_width(), 23))
+        self.screen.blit(self.assets["display_glas"], (557, 26))
         pg.display.update()
 
-    def main(self) -> None:
-        print(self.beat_buttons)    
+    def main(self) -> None:   
         while self.run:
             self.calculate_delta_time()
             self.calculate_fps()
             self.beat_duration = self.calculate_beat_times()
+            self.check_button_collisions()
+            for light in self.slot_lights:
+                light.update()
             
-            if self.play:
-                self.play_instruments()
+            
+            if self.state == "play" or self.state == "pause":                
+                self.de_activate_slot_lights()
 
+            if self.state == "play":
+                self.play_instruments()
                 self.shift = self.sum_beat_time()
                 self.slot_shifter()
 
--- utils.py
+++ utils.py
@@ -1,24 +1,29 @@
-import os
 import pygame as pg
 
 
 def load_image(img_name: str) -> pg.Surface:
     return pg.image.load("images/" + img_name + ".png")
 
-
 def scale_image(image: str, scalefactor: float) -> pg.Surface:
     return pg.transform.scale(image, (int(image.get_width() * scalefactor), int(image.get_height() * scalefactor)))
 
-def create_beat_button_pattern(program: object):
+def create_beat_button_pattern(program: object) -> list:
     beat_button_pattern = []
     for i in range(4):
         help_list = []
         for j in range(4):
             for k in range(4):
                 b_type = "button1" if k == 0 else "button234"
-                help_list.append(Button(prog=program, button_type=b_type, pos=(i, j + k), mirror=False, activatable=True))
+                help_list.append(Button(prog=program, button_type=b_type, pos=(222 + k * 51 + j * 202 , 119 + i * 97), mirror=False, activatable=True))
         beat_button_pattern.append(help_list)
     return beat_button_pattern
+
+def create_slot_light_list(program: object) -> list:
+    slot_light_list: list = []
+    for i in range(16):
+        slot_light_list.append(SlotLight(program=program, pos=(225 + i * 51, 78)))
+    return slot_light_list
+
 
 class Button:
     def __init__(self, prog: object, button_type: str, pos: tuple, mirror: bool = False, activatable: bool = False) -> None:
@@ -26,39 +31,51 @@ class Button:
         self.button_type: str = button_type
         self.pos: tuple = pos
         self.active: bool = True if self.button_type == "stop" else False
-        self.activatable = activatable
-        if self.activatable:
-            if not self.active:
-                asset_string = f"{self.button_type}/inactive"
-            else:
-                asset_string = f"{self.button_type}/active"
-        else:
-            asset_string = self.button_type
-        self.image: pg.image = self.prog.assets[asset_string]
-        self.rect: pg.rect = self.image.get_rect(center=self.pos)
+        self.state: str = "active" if self.button_type == "stop" else "inactive"
+        self.activatable = activatable       
+        asset_string = f"{self.button_type}/{self.state}" if self.activatable else self.button_type
+        self.image: pg.image = pg.transform.flip(self.prog.assets[asset_string], mirror, False)
+        self.rect: pg.rect = self.image.get_rect(topleft=self.pos)
         self.clicked: bool = False
 
     def is_active(self) -> bool:
         return self.active
     
-    def check_collision(self):
+    def switch_state(self) -> None:
+        self.active = not self.active
+        self.state = "active" if self.active else "inactive"
+        self.image: pg.image = self.prog.assets[self.button_type + "/" + self.state]
+    
+    def check_collision(self) -> bool:
         mouse_pos = pg.mouse.get_pos()
         if self.rect.collidepoint(mouse_pos):
             if pg.mouse.get_pressed()[0]:
                 self.clicked = True
-            else:
-                self.clicked = False
+
             if not pg.mouse.get_pressed()[0] and self.clicked:
-                if self.activatable:
-                    self.active = not self.active
-                else:
-                    return True
+                if self.activatable:          
+                    self.switch_state()
                 self.clicked = False
-                
+                return True           
         else:
             self.clicked = False
 
-    def render(self, surf) -> None:
+    def render(self, surf: pg.surface) -> None:
         surf.blit(self.image, self.pos)
 
-        
+
+class SlotLight:
+    def __init__(self, program: object, pos: tuple) -> None:
+        self.prog: object = program
+        self.pos: tuple = pos
+        self.active: bool = False
+        self.state: str = "inactive"
+        self.image: pg.surface = self.prog.assets[f"slot light/{self.state}"]
+
+    def update(self, activated: bool=False) -> None:
+        self.active = True if activated else False
+        self.state = "active" if activated else "inactive"   
+        self.image = self.prog.assets[f"slot light/{self.state}"]
+
+    def render(self, surf: pg.Surface) -> None:
+        surf.blit(self.image, self.pos)
