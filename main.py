b99b451ca6c99df4e52b95c5d71afdd4309908ac
CaGListRo <paco.nyns@gmail.com>
2024-05-03 17:34:30 +0200

    WIP: Implemented sounds, started to implement the volume sliders.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import load_image, scale_image, create_beat_button_pattern, create_slot_light_list, Button
+from utils import load_image, scale_image, load_sound, create_beat_button_pattern, create_slot_light_list, create_sliders, Button
 
 import pygame as pg
 from time import time
@@ -6,15 +6,17 @@ from time import time
 class BeatMachine:
     def __init__(self) -> None:
         pg.init()
+        pg.mixer.init()
+        pg.mixer.set_num_channels(16)
         self.screen = pg.display.set_mode((1280, 720))
         self.fps: int = 0
         self.run: bool = True
 
         self.bpm: int = 120
         self.number_of_beats: int = 16
-        self.active_instrument_slot: int = 0
+        self.active_instrument_slot: int = -1
         self.beat_duration: float = self.calculate_beat_times()
-        self.beat_time: float = 0
+        self.beat_time: float = 60
         self.state: str = "stop"
         self.shift: bool = False
         self.font = pg.font.SysFont("arial", 40)
@@ -28,7 +30,7 @@ class BeatMachine:
                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],]        
         
-        self.assets: dict = {
+        self.images: dict = {
             "body": scale_image(load_image(img_name="body"), scalefactor=0.55),
             "bpm +- 1": scale_image(load_image(img_name="bpm plus minus 1"), scalefactor=0.36),
             "bpm +- 10": scale_image(load_image(img_name="bpm plus minus 10"), scalefactor=0.36),
@@ -48,8 +50,18 @@ class BeatMachine:
             "display_glas": scale_image(load_image(img_name="display glas"), scalefactor=0.44),
         }
 
+        self.sounds: dict = {
+            "clap/one": load_sound(file_path="kit1", sound_name="clap"),
+            "crash/one": load_sound(file_path="kit1", sound_name="crash"),
+            "hihat/one": load_sound(file_path="kit1", sound_name="hi hat"),
+            "kick/one": load_sound(file_path="kit1", sound_name="kick"),
+            "snare/one": load_sound(file_path="kit1", sound_name="snare"),
+            "tom/one": load_sound(file_path="kit1", sound_name="tom"),
+        }
+
         self.beat_buttons = create_beat_button_pattern(self)
         self.slot_lights = create_slot_light_list(self)
+        self.sliders = create_sliders(self)
 
         self.play_button = Button(prog=self, button_type="play", pos=(988, 31), activatable=True)
         self.pause_button = Button(prog=self, button_type="pause", pos=(931, 31), activatable=True)
@@ -71,9 +83,17 @@ class BeatMachine:
             self.shift = False
             if self.active_instrument_slot > self.number_of_beats - 1:
                 self.active_instrument_slot = 0
-
+            self.play_instruments()
+            
     def play_instruments(self) -> None:
-        pass
+        if self.beat_buttons[0][self.active_instrument_slot].is_active():
+            self.sounds["clap/one"].play()
+        if self.beat_buttons[1][self.active_instrument_slot].is_active():
+            self.sounds["snare/one"].play()
+        if self.beat_buttons[2][self.active_instrument_slot].is_active():
+            self.sounds["kick/one"].play()
+        if self.beat_buttons[3][self.active_instrument_slot].is_active():
+            self.sounds["crash/one"].play()
 
     def sum_beat_time(self) -> bool:
         self.beat_time += self.dt
@@ -94,7 +114,18 @@ class BeatMachine:
         self.dt = time() - self.last_time
         self.last_time = time()
 
-    def check_button_collisions(self):
+    def set_volume(self) -> None:
+        pg.mixer.music.set_volume(self.sliders[0].get_value())
+        self.sounds["clap/one"].set_volume(self.sliders[1].get_value())
+        self.sounds["snare/one"].set_volume(self.sliders[2].get_value())
+        self.sounds["kick/one"].set_volume(self.sliders[3].get_value())
+        self.sounds["crash/one"].set_volume(self.sliders[4].get_value())
+
+    def check_slider_collisions(self) -> None:
+        for slider in self.sliders:
+            slider.check_collision()
+
+    def check_button_collisions(self) -> None:
         for button_list in self.beat_buttons:
             for btn in button_list:
                 btn.check_collision()
@@ -110,7 +141,8 @@ class BeatMachine:
             if self.pause_button.is_active(): self.pause_button.switch_state()
             if self.play_button.is_active(): self.play_button.switch_state()
             self.state = "stop"
-            self.active_instrument_slot = 0
+            self.active_instrument_slot = -1
+            self.beat_time: float = 60
         if self.bpm_minus_ten_button.check_collision(): self.bpm -= 10
         if self.bpm_minus_one_button.check_collision(): self.bpm -= 1
         if self.bpm_plus_ten_button.check_collision(): self.bpm += 10
@@ -122,6 +154,8 @@ class BeatMachine:
                 btn.render(self.screen)
         for light in self.slot_lights:
             light.render(self.screen)
+        for slider in self.sliders:
+            slider.render(self.screen)
         self.play_button.render(self.screen)
         self.pause_button.render(self.screen)
         self.stop_button.render(self.screen)
@@ -132,12 +166,12 @@ class BeatMachine:
 
     def draw_window(self) -> None:
         pg.display.set_caption(f"     Beat Machine     FPS: {self.fps}")
-        self.screen.blit(self.assets["body"], (0,0))
+        self.screen.blit(self.images["body"], (0,0))
         self.render_buttons()
         
         bpm_to_blit = self.font.render(str(self.bpm), False, "green")
         self.screen.blit(bpm_to_blit, (680 - bpm_to_blit.get_width(), 23))
-        self.screen.blit(self.assets["display_glas"], (557, 26))
+        self.screen.blit(self.images["display_glas"], (557, 26))
         pg.display.update()
 
     def main(self) -> None:   
@@ -146,15 +180,15 @@ class BeatMachine:
             self.calculate_fps()
             self.beat_duration = self.calculate_beat_times()
             self.check_button_collisions()
+            self.check_slider_collisions()
+            self.set_volume()
             for light in self.slot_lights:
                 light.update()
-            
-            
+                    
             if self.state == "play" or self.state == "pause":                
                 self.de_activate_slot_lights()
 
             if self.state == "play":
-                self.play_instruments()
                 self.shift = self.sum_beat_time()
                 self.slot_shifter()
 
--- utils.py
+++ utils.py
@@ -7,6 +7,9 @@ def load_image(img_name: str) -> pg.Surface:
 def scale_image(image: str, scalefactor: float) -> pg.Surface:
     return pg.transform.scale(image, (int(image.get_width() * scalefactor), int(image.get_height() * scalefactor)))
 
+def load_sound(file_path: str, sound_name: str) -> pg.mixer:
+    return pg.mixer.Sound("sounds/" + file_path + "/" + sound_name + ".wav")
+
 def create_beat_button_pattern(program: object) -> list:
     beat_button_pattern = []
     for i in range(4):
@@ -24,6 +27,13 @@ def create_slot_light_list(program: object) -> list:
         slot_light_list.append(SlotLight(program=program, pos=(225 + i * 51, 78)))
     return slot_light_list
 
+def create_sliders(program: object) -> list:
+    slider_list: list = []
+    slider_list.append(Slider(program=program, min=110, max=344, current_value_in_percent=80, y_pos=47))
+    for i in range(4):
+        slider_list.append(Slider(program=program, min=75, max=190, current_value_in_percent=80, y_pos=163 + i * 97))
+    return slider_list
+
 
 class Button:
     def __init__(self, prog: object, button_type: str, pos: tuple, mirror: bool = False, activatable: bool = False) -> None:
@@ -34,7 +44,7 @@ class Button:
         self.state: str = "active" if self.button_type == "stop" else "inactive"
         self.activatable = activatable       
         asset_string = f"{self.button_type}/{self.state}" if self.activatable else self.button_type
-        self.image: pg.image = pg.transform.flip(self.prog.assets[asset_string], mirror, False)
+        self.image: pg.image = pg.transform.flip(self.prog.images[asset_string], mirror, False)
         self.rect: pg.rect = self.image.get_rect(topleft=self.pos)
         self.clicked: bool = False
 
@@ -44,7 +54,7 @@ class Button:
     def switch_state(self) -> None:
         self.active = not self.active
         self.state = "active" if self.active else "inactive"
-        self.image: pg.image = self.prog.assets[self.button_type + "/" + self.state]
+        self.image: pg.image = self.prog.images[self.button_type + "/" + self.state]
     
     def check_collision(self) -> bool:
         mouse_pos = pg.mouse.get_pos()
@@ -64,18 +74,47 @@ class Button:
         surf.blit(self.image, self.pos)
 
 
+class Slider:
+    def __init__(self, program: object, min: int, max: int, current_value_in_percent: int, y_pos: int) -> None:
+        self.prog: object = program
+        self.min: int = min
+        self.max: int = max
+        self.range: int = self.max - self.min
+        self.val: int = current_value_in_percent * self.range / 100
+        self.image: pg.surface = self.prog.images["slider knob"]        
+        self.y_pos: int = y_pos - self.image.get_height() // 2
+        self.pos: list = [self.min + self.val - self.image.get_width() // 2, self.y_pos]
+        self.rect: pg.rect = self.image.get_rect(topleft=self.pos)
+
+    def check_collision(self) -> None:
+        mouse_pos = pg.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            if pg.mouse.get_pressed()[0]:
+                self.pos[0] = mouse_pos[0]
+                if self.pos[0] < self.min:
+                    self.pos[0] = self.min
+                elif self.pos[0] > self.max - self.image.get_width():
+                    self.pos[0] = self.max - self.image.get_width()
+
+    def get_value(self) -> float:
+        return (self.pos[0] - self.min) / self.range
+
+    def render(self, surf: pg.surface) -> None:
+        surf.blit(self.image, self.pos)
+
+
 class SlotLight:
     def __init__(self, program: object, pos: tuple) -> None:
         self.prog: object = program
         self.pos: tuple = pos
         self.active: bool = False
         self.state: str = "inactive"
-        self.image: pg.surface = self.prog.assets[f"slot light/{self.state}"]
+        self.image: pg.surface = self.prog.images[f"slot light/{self.state}"]
 
     def update(self, activated: bool=False) -> None:
         self.active = True if activated else False
         self.state = "active" if activated else "inactive"   
-        self.image = self.prog.assets[f"slot light/{self.state}"]
+        self.image = self.prog.images[f"slot light/{self.state}"]
 
     def render(self, surf: pg.Surface) -> None:
         surf.blit(self.image, self.pos)
