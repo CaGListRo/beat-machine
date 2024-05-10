a4b9798004e05464a0b583a3c27ccdd34d6f01f6
CaGListRo <paco.nyns@gmail.com>
2024-05-10 15:32:45 +0200

    WIP: Made the sliders work better.

--- elements.py
+++ elements.py
@@ -62,20 +62,19 @@ class Slider:
         if self.rect.collidepoint(mouse_pos):
             self.collide = True
         if pg.mouse.get_pressed()[0] and self.collide:
-            new_x = mouse_pos[0] - self.prog.body_surf_pos[0] - self.image.get_width() // 2
-            if new_x < self.min:
-                new_x = self.min
-            elif new_x > self.max - self.image.get_width():
-                new_x = self.max - self.image.get_width()
-            self.pos[0] = new_x
-            self.rect.topleft = (self.prog.body_surf_pos[0] + self.pos[0], self.prog.body_surf_pos[1] + self.pos[1])
-            self.val = (new_x - self.min) / self.range
+            self.pos[0] = mouse_pos[0] - self.prog.body_surf_pos[0] - self.image.get_width() // 2  # Anpassung hier
+            if self.pos[0] < self.min:
+                self.pos[0] = self.min
+            elif self.pos[0] > self.max - self.image.get_width() // 2:
+                self.pos[0] = self.max - self.image.get_width() // 2
+            self.rect.topleft = (self.prog.body_surf_pos[0] + self.pos[0], self.prog.body_surf_pos[1] + self.y_pos)
+            self.val = (self.pos[0] - self.min) / self.range
         if not pg.mouse.get_pressed()[0]:
             self.collide = False
             self.rect.topleft = (self.prog.body_surf_pos[0] + self.pos[0], self.prog.body_surf_pos[1] + self.pos[1])
 
     def get_value(self) -> float:
-        return round((self.pos[0] - self.min) / self.range, 2)
+        return round(self.val, 2)
 
     def render(self, surf: pg.surface) -> None:
         surf.blit(self.image, self.pos)
